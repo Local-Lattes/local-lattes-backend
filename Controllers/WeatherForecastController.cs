de9710d56dc797eca8354a8f5a9f10ad9edbceae
Exotica0122 <anpeteran2@gmail.com>
2024-05-23 20:44:33 +1200

    remove debug log

--- Controllers/WeatherForecastController.cs
+++ Controllers/WeatherForecastController.cs
@@ -28,6 +28,5 @@ public class WeatherForecastController : ControllerBase
             Summary = Summaries[Random.Shared.Next(Summaries.Length)]
         })
         .ToArray();
-        Debug.Log("debug");
     }
 }
